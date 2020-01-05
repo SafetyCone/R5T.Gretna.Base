bb146d1778a1bb70748d6c224d7fc86d5a035810
MinexAutomation <minex.automation@gmail.com>
2020-01-05 14:20:58 -0800

    Improved repository methods to be asynchronous.

--- source/R5T.Gretna.Base/Code/Services/Interfaces/IImageFileRepository.cs
+++ source/R5T.Gretna.Base/Code/Services/Interfaces/IImageFileRepository.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading.Tasks;
 
 using R5T.Francia;
 using R5T.Philippi;
@@ -10,16 +11,16 @@ namespace R5T.Gretna
 {
     public interface IImageFileRepository
     {
-        ImageFileIdentity AddImage(Stream imageFileStream, FileName imageFileName, FileFormat fileFormat);
+        Task<ImageFileIdentity> AddImage(Stream imageFileStream, FileName imageFileName, FileFormat fileFormat);
 
-        bool Exists(ImageFileIdentity imageFileIdentity);
+        Task<bool> Exists(ImageFileIdentity imageFileIdentity);
 
-        Stream GetImageFileStream(ImageFileIdentity imageFileIdentity);
-        FileName GetImageFileName(ImageFileIdentity imageFileIdentity);
-        FileFormat GetImageFileFormat(ImageFileIdentity imageFileIdentity);
+        Task<Stream> GetImageFileStream(ImageFileIdentity imageFileIdentity);
+        Task<FileName> GetImageFileName(ImageFileIdentity imageFileIdentity);
+        Task<FileFormat> GetImageFileFormat(ImageFileIdentity imageFileIdentity);
 
-        ImageFileReadInfo GetReadInfo(ImageFileIdentity imageFileIdentity);
+        Task<ImageFileReadInfo> GetReadInfo(ImageFileIdentity imageFileIdentity);
 
-        void Delete(ImageFileIdentity imageFileIdentity);
+        Task Delete(ImageFileIdentity imageFileIdentity);
     }
 }
