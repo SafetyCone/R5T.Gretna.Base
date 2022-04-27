5a74d1105f182939c29163c58b4116ba0aeeeb8d
davidcoats <david@getsafetycone.com>
2022-04-27 15:31:35 -0700

    Interim changes.

---
+++ source/R5T.Gretna.Base/Code/Services/Definitions/IImageFileRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using R5T.Francia;
+using R5T.Philippi;
+using R5T.Sparta;
+
+using R5T.T0064;
+
+
+namespace R5T.Gretna
+{
+    [ServiceDefinitionMarker]
+    public interface IImageFileRepository : IServiceDefinition
+    {
+        Task<ImageFileIdentity> AddImage(Stream imageFileStream, FileName imageFileName, FileFormat fileFormat);
+
+        Task<bool> Exists(ImageFileIdentity imageFileIdentity);
+
+        Task<Stream> GetImageFileStream(ImageFileIdentity imageFileIdentity);
+        Task<FileName> GetImageFileName(ImageFileIdentity imageFileIdentity);
+        Task<FileFormat> GetImageFileFormat(ImageFileIdentity imageFileIdentity);
+
+        Task SetImageData(ImageFileIdentity imageFileIdentity, Stream imageFileStream);
+
+        Task<ImageFileReadInfo> GetReadInfo(ImageFileIdentity imageFileIdentity);
+
+        Task Delete(ImageFileIdentity imageFileIdentity);
+    }
+}
